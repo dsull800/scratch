3c07919f3bdb5817867e84a02c91a3787b3e51f2
dsull800 <daniel.sullivan800@gmail.com>
2023-12-17 17:43:47 -0800

    copied some solution for 137

---
+++ solutions/137.py
@@ -0,0 +1,7 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        ones, twos = 0, 0
+        for i in range(len(nums)):
+            ones = (ones ^ nums[i]) & ~twos
+            twos = (twos ^ nums[i]) & ~ones
+        return ones
